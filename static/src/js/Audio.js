0f2d4d32119889c262a2bd53ca837da0f7acb5df
FGV26 <145302993+FGV26@users.noreply.github.com>
2023-10-12 23:26:36 -0500

    final de musica con javascript
    fghfh

--- static/src/js/Audio.js
+++ static/src/js/Audio.js
@@ -1,23 +1,35 @@
 let audioPlaying = null;
+let restart = false;
 
 function reproducir(audioElement, startTime) {
     const audio = audioElement;
     if (audioPlaying && audioPlaying !== audio) {
         audioPlaying.pause();
     }
-    if (audio.paused) {
+    if (audio.paused || restart) {
         audio.currentTime = startTime;
         audio.play();
         audioPlaying = audio;
+        restart = false;
+
+        const duracionMaxima = 35;
+        audio.ontimeupdate = function() {
+            if (audio.currentTime >= startTime + duracionMaxima) {
+                audio.pause();
+                audio.currentTime = startTime; 
+                audioPlaying = null;
+            }
+        };
     }
 }
 
 function reiniciar(audioElement, startTime) {
     const audio = audioElement;
-    if (audio === audioPlaying) {
-        audio.pause();
-        audio.currentTime = startTime;
+    audio.currentTime = startTime;
+    if (audio.paused) {
+        restart = true;
         audio.play();
+        audioPlaying = audio;
     }
 }
 
@@ -25,9 +37,8 @@ function pausar(audioElement) {
     const audio = audioElement;
     if (audio === audioPlaying) {
         audio.pause();
+        audio.currentTime = audio.currentTime - 0.01; 
         audioPlaying = null;
     }
 }
-
-
 
